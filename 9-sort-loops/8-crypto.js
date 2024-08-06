eb9b3a32b5ec9a7327ef19fbbfe2ed770accd3d3
kolbassa64 <kalantaewskiy_gmail.com>
2024-08-07 00:08:07 +0300

    Revert "corrections"
    This reverts commit cb8cd4df2259b1830a1981a0c0d0603d97494f85.

--- 9-sort-loops/8-crypto.js
+++ 9-sort-loops/8-crypto.js
@@ -1,23 +1,29 @@
-function descendingorder(value, key){
+function ascendingorder (value) {
   for (let i = 0; i < value.length; i++) {
     for (let j = 0; j < value.length - i - 1; j++) {
-      switch (key){
-      case 1 :
-      if (value[j] < value[j + 1]) {
-        let temp = value[j];
-        value[j] = value[j + 1];
-        value[j + 1] = temp;
-        break
-      }
-      case 2 :
       if (value[j] > value[j + 1]) {
         let temp = value[j];
         value[j] = value[j + 1];
         value[j + 1] = temp;
-        break;
       }
     }
-    }
+  }
+  return value;
 }
-return value;
+function descendingorder(value){
+  for (let i = 0; i < value.length; i++) {
+    for (let j = 0; j < value.length - i - 1; j++) {
+      if (value[j] < value[j + 1]) {
+        let temp = value[j];
+        value[j] = value[j + 1];
+        value[j + 1] = temp;
+      }
+    }
+  }
+  return value;
+}
+
+function keysorting (value, key){
+  key ? ascendingorder(value) : descendingorder(value); // такой вариант мне показался более читаемым, куча if-ов мне не оч
+  return value;
 }
