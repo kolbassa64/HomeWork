8e6b25a8f5a3e043c2a0f453c06e2847af9c2d5a
kolbassa64 <kalantaewskiy_gmail.com>
2024-08-07 00:36:01 +0300

    correct

--- 9-sort-loops/8-crypto.js
+++ 9-sort-loops/8-crypto.js
@@ -1,29 +1,21 @@
-function ascendingorder (value) {
+function ascendingorder (value, key) {
   for (let i = 0; i < value.length; i++) {
     for (let j = 0; j < value.length - i - 1; j++) {
-      if (value[j] > value[j + 1]) {
-        let temp = value[j];
-        value[j] = value[j + 1];
-        value[j + 1] = temp;
+      switch (key){
+        case true:
+        if (value[j] > value[j + 1]) {
+          let temp = value[j];
+          value[j] = value[j + 1];
+          value[j + 1] = temp;
+        }
+        case false:
+        if (value[j] < value[j + 1]) {
+          let temp = value[j];
+          value[j] = value[j + 1];
+          value[j + 1] = temp;
+        }
       }
     }
   }
-  return value;
-}
-function descendingorder(value){
-  for (let i = 0; i < value.length; i++) {
-    for (let j = 0; j < value.length - i - 1; j++) {
-      if (value[j] < value[j + 1]) {
-        let temp = value[j];
-        value[j] = value[j + 1];
-        value[j + 1] = temp;
-      }
-    }
-  }
-  return value;
-}
-
-function keysorting (value, key){
-  key ? ascendingorder(value) : descendingorder(value); // такой вариант мне показался более читаемым, куча if-ов мне не оч
   return value;
 }
